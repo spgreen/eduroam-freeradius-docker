63c2f369da5c3768b33b91e041a6d299db1e61de
spgreen <simonpetergreen@singaren.net.sg>
2015-12-16 17:41:58 +0800

    Test/Production Environment Split

--- files/run.sh
+++ files/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+ENV1="PRODUCTION"
+ENV2="TEST"
+
 sed -i -e "s/EDUROAM_FLR1/$EDUROAM_FLR1/g" -e "s/EDUROAM_FLR2/$EDUROAM_FLR2/g" -e "s/FLR_EDUROAM_SECRET/$FLR_EDUROAM_SECRET/g" \
 	/etc/raddb/clients.conf
 
@@ -18,5 +21,10 @@ sed -i -e "s/YOUR_REALM/$YOUR_REALM/g" /etc/raddb/sites-enabled/eduroam
 sed -i -e "s/YOUR_REALM/$YOUR_REALM/g" -e "s/TEST_PASSWORD/$TEST_PASSWORD/g" /etc/raddb/mods-config/files/authorize
 
 
-
-/usr/local/raddb/sbin/radiusd -xx -l /var/log/freeradius/radius.log -f
+if [ "$ENVIRONMENT" = "$ENV1" ]; then
+	/usr/local/raddb/sbin/radiusd -f 
+elif [ "$ENVIRONMENT" = "$ENV2" ]; then
+ 	/usr/local/raddb/sbin/radiusd -X -l /var/log/freeradius/radius.log -f
+else
+ 	echo ERROR
+fi
--- restart_eduroamFreeRADIUS.sh
+++ restart_eduroamFreeRADIUS.sh
@@ -5,10 +5,12 @@ EDUROAM_FLR2=192.168.100.110
 FLR_EDUROAM_SECRET=supertest
 YOUR_REALM=docker.sg
 TEST_PASSWORD=docker123
+ENVIRONMENT=TEST #TEST or PRODUCTION
 
+ 
 docker stop freeradius-eduroam; docker rm freeradius-eduroam
 docker run -d --name freeradius-eduroam -v /etc/localtime:/etc/localtime:ro -p 1812:1812/udp -p 1813:1813/udp \
   -e EDUROAM_FLR1=$EDUROAM_FLR1 -e EDUROAM_FLR2=$EDUROAM_FLR2 -e FLR_EDUROAM_SECRET=$FLR_EDUROAM_SECRET \
-  -e YOUR_REALM=$YOUR_REALM -e  TEST_PASSWORD=$TEST_PASSWORD \
+  -e YOUR_REALM=$YOUR_REALM -e  TEST_PASSWORD=$TEST_PASSWORD -e ENVIRONMENT=$ENVIRONMENT \
   spgreen/freeradius-eduroam
 
