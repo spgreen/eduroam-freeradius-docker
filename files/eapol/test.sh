2529ed9f57cbcd9197cf251dd0000bf08675ea4c
spgreen <simonpetergreen@singaren.net.sg>
2015-12-25 00:58:11 +0800

    Update test.sh
    Added EDUROAM_FLR variables to be controlled by restart.sh and run.sh

--- files/eapol/test.sh
+++ files/eapol/test.sh
@@ -9,11 +9,11 @@ sed -e "s/USERNAME/$USERNAME/" -e "s/PASSWORD/$PASSWORD/" test.conf.template > t
 
 if [ "$FLR_SELECT" = 1 ]; then
 
-    eapol_test -c test.conf -a 192.168.100.102 -p 1812 -s supertest
+    eapol_test -c test.conf -a EDUROAM_FLR1 -p 1812 -s supertest
 
 elif [ "$FLR_SELECT" = 2 ]; then
 
-    eapol_test -c test.conf -a 192.168.100.110 -p 1812 -s supertest
+    eapol_test -c test.conf -a EDUROAM_FLR2 -p 1812 -s supertest
 
 else
     echo
