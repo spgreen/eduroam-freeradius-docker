7497d95cbfe0cbcae672903adb6020a4751800f0
spgreen <simonpetergreen@singaren.net.sg>
2016-01-07 16:14:46 +0800

    Update adding single FLR server option

--- files/run.sh
+++ files/run.sh
@@ -3,24 +3,33 @@
 ENV1="PRODUCTION"
 ENV2="TEST"
 
+#Changes the variables to the ones in the restart script
+#FLR Server settings
 sed -i -e "s/EDUROAM_FLR1/$EDUROAM_FLR1/g" -e "s/EDUROAM_FLR2/$EDUROAM_FLR2/g" -e "s/FLR_EDUROAM_SECRET/$FLR_EDUROAM_SECRET/g" \
 	/etc/raddb/clients.conf
 
+#FLR Proxy Settings
 sed -i -e "s/EDUROAM_FLR1/$EDUROAM_FLR1/g" -e "s/EDUROAM_FLR2/$EDUROAM_FLR2/g" -e "s/FLR_EDUROAM_SECRET/$FLR_EDUROAM_SECRET/g" \
 	/etc/raddb/proxy.conf
-
 sed -i -e "s/YOUR_REALM/$YOUR_REALM/g" /etc/raddb/proxy.conf
-
-
-#sed -i -e "s/EDUROAM_FLR1/$EDUROAM_FLR1/g" -e "s/FLR_EDUROAM_SECRET/$FLR_EDUROAM_SECRET/g" /root/test.sh
-sed -i -e "s/EDUROAM_FLR1/$EDUROAM_FLR1/g" -e "s/EDUROAM_FLR2/$EDUROAM_FLR2/g" -e "s/FLR_EDUROAM_SECRET/$FLR_EDUROAM_SECRET/g"  \
-	/root/test.sh
 
 sed -i -e "s/YOUR_REALM/$YOUR_REALM/g" /etc/raddb/sites-enabled/eduroam
 
+#Configures the test.sh script with the FLR IP addresses and the FLR secret via the restart script
+sed -i -e "s/EDUROAM_FLR1/$EDUROAM_FLR1/g" -e "s/EDUROAM_FLR2/$EDUROAM_FLR2/g" -e "s/FLR_EDUROAM_SECRET/$FLR_EDUROAM_SECRET/g"  \
+	/root/test.sh
+
+#Sets up the testuser account via the restart script
 sed -i -e "s/YOUR_REALM/$YOUR_REALM/g" -e "s/TEST_PASSWORD/$TEST_PASSWORD/g" /etc/raddb/mods-config/files/authorize
 
 
+#Configures the number of FLR Servers between 1 to 2. If 1 is selected in the restart script, the 2nd FLR Variables will be commented out
+if [ "$NO_OF_FLR_SERVERS" = 1 ]; then
+	sed -i -e '304,310 s/^/#/' /etc/raddb/clients.conf
+	sed -i -e '13,18 s/^/#/' /etc/radddb/proxy.conf
+fi
+
+#Configures the environment (TEST or PRODUCTION) 
 if [ "$ENVIRONMENT" = "$ENV1" ]; then
 	/usr/local/raddb/sbin/radiusd -f 
 elif [ "$ENVIRONMENT" = "$ENV2" ]; then
--- restart_eduroamFreeRADIUS.sh
+++ restart_eduroamFreeRADIUS.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 
+NO_OF_FLR_SERVERS=2 #Select the number of FLR servers that your eduroam setup uses. Select either 1 or 2
 EDUROAM_FLR1=192.168.100.102
 EDUROAM_FLR2=192.168.100.110
 FLR_EDUROAM_SECRET=supertest
-YOUR_REALM=docker.sg
+YOUR_REALM=pern.edu.pk
 TEST_PASSWORD=docker123
 ENVIRONMENT=TEST #TEST or PRODUCTION
 
- 
-docker stop freeradius-eduroam; docker rm freeradius-eduroam
-docker run -d --name freeradius-eduroam -v /etc/localtime:/etc/localtime:ro -p 1812:1812/udp -p 1813:1813/udp \
-  -e EDUROAM_FLR1=$EDUROAM_FLR1 -e EDUROAM_FLR2=$EDUROAM_FLR2 -e FLR_EDUROAM_SECRET=$FLR_EDUROAM_SECRET \
-  -e YOUR_REALM=$YOUR_REALM -e  TEST_PASSWORD=$TEST_PASSWORD -e ENVIRONMENT=$ENVIRONMENT \
-  spgreen/freeradius-eduroam
 
+
+
+
+if [ $NO_OF_FLR_SERVERS = 1 ] || [ $NO_OF_FLR_SERVERS = 2 ]; then
+	if [  $ENVIRONMENT = "TEST" ] || [  $ENVIRONMENT = "PRODUCTION" ]; then 
+ 
+		docker stop freeradius-eduroam; docker rm freeradius-eduroam
+		docker run -d --name freeradius-eduroam -v /etc/localtime:/etc/localtime:ro -p 1812:1812/udp -p 1813:1813/udp \
+  			-e EDUROAM_FLR1=$EDUROAM_FLR1 -e EDUROAM_FLR2=$EDUROAM_FLR2 -e FLR_EDUROAM_SECRET=$FLR_EDUROAM_SECRET \
+  			-e YOUR_REALM=$YOUR_REALM -e  TEST_PASSWORD=$TEST_PASSWORD -e ENVIRONMENT=$ENVIRONMENT -e NO_OF_FLR_SERVERS=$NO_OF_FLR_SERVERS \
+  			spgreen/freeradius-eduroam
+	else
+		echo
+		echo "Please enter either 'TEST' or 'PRODUCTION' for the ENVIRONMENT variable"
+		echo "TEST environment produces debug logs found in /var/log/freeradius/radius.log"
+		echo
+	fi
+else
+	echo
+	echo "Please enter either '1' or '2' for the NO_OF_FLR_SERVERS variable"
+	echo "This will set the number of FLR servers you have within your eduroam setup"
+	echo
+fi
