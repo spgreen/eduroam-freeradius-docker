5d613ddb03e88069fc5d0274eeeb7074d5a67de7
Tein Administrator <admin@sg-nrad1.tein.aarnet.edu.au>
2018-07-30 13:48:05 +1000

    Added tzdata package to allow for timezone changes
    Previous method of updating /etc/localtime no longer works
    in Alpine 3.7 Base Docker image, thus the tzdata package
    needs to be installed to allow the user to change the timezone
    by change the TZ environment variable on container start up.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM alpine:3.7
 MAINTAINER Simon Green <simonpetergreen@singaren.net.sg>
 
-RUN apk update && apk upgrade 
+ENV TZ UTC
+
+RUN apk update && apk upgrade
 
 #Installs necessary dependencies for compiling FreeRADIUS and other useful tools such as vim and tcpdump
 RUN apk add freeradius freeradius-eap freeradius-ldap freeradius-krb5 \ 
-            tcpdump wpa_supplicant bash grep bind-tools
+            tcpdump wpa_supplicant bash grep bind-tools tzdata
 
 #Copies the necessary configs to set up the FreeRADIUS Server for eduroam use
 COPY files/environment/ /
--- restart_eduroamFreeRADIUS.sh
+++ restart_eduroamFreeRADIUS.sh
@@ -7,6 +7,7 @@ FLR_EDUROAM_SECRET=sgsecret
 YOUR_REALM=docker.edu.sg
 TEST_PASSWORD=docker123
 ENVIRONMENT=TEST #TEST or PRODUCTION
+TZ=UTC
 
 
 
@@ -16,7 +17,7 @@ if [ $NO_OF_FLR_SERVERS = 1 ] || [ $NO_OF_FLR_SERVERS = 2 ]; then
 		docker stop freeradius-eduroam; docker rm freeradius-eduroam
 		docker run -it  --name freeradius-eduroam -v /etc/localtime:/etc/localtime:ro -p 1812:1812/udp -p 1813:1813/udp \
   			-e EDUROAM_FLR1=$EDUROAM_FLR1 -e EDUROAM_FLR2=$EDUROAM_FLR2 -e FLR_EDUROAM_SECRET=$FLR_EDUROAM_SECRET \
-  			-e YOUR_REALM=$YOUR_REALM -e  TEST_PASSWORD=$TEST_PASSWORD -e ENVIRONMENT=$ENVIRONMENT -e NO_OF_FLR_SERVERS=$NO_OF_FLR_SERVERS \
+  			-e YOUR_REALM=$YOUR_REALM -e  TEST_PASSWORD=$TEST_PASSWORD -e TZ=$TZ -e ENVIRONMENT=$ENVIRONMENT -e NO_OF_FLR_SERVERS=$NO_OF_FLR_SERVERS \
                         spgreen/freeradius-eduroam
 	else
 		echo
